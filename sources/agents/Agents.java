d1e4289f89dee8971cf79eb78cf3c4906c1cb662
otttisss <otttisss@yandex.ru>
2023-05-16 21:42:57 +0300

    readConfig added

--- sources/agents/Agents.java
+++ sources/agents/Agents.java
@@ -3,6 +3,11 @@ package agents;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Agents {
     private static Map<String, Double> metrics = new HashMap<>();
@@ -10,8 +15,43 @@ public class Agents {
     private static final List<String> vectorOperators = List.of("<", "<=", "==", ">=", ">");
     private static final String alphaNum = "0123456789.";
 
-    public static void readConfig(String path) {
+    public static void readConfig(String path) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String line;
 
+        while ((line = reader.readLine()) != null) {
+            String[] tokens = line.split("\\s+");
+            if (tokens.length < 3) {
+                throw new RuntimeException("Error: bad line!");
+            }
+            String key = tokens[0];
+            String sOperator = tokens[1];
+            String sMeaning = tokens[3];
+
+            if (!containsOperator(sOperator))
+                throw new RuntimeException("Error: bad line!" + sOperator);
+            if (!isAlphaNumeric(sMeaning))
+                throw new RuntimeException("Error: bad line!" + sMeaning);
+            configMap.put(key, Map.of(sOperator, Double.parseDouble(sMeaning)));
+        }
+        reader.close();
+    }
+    private static boolean containsOperator(String operator) {
+        for (String op : vectorOperators) {
+            if (op.equals(operator)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isAlphaNumeric(String s) {
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c) && c != '.') {
+                return false;
+            }
+        }
+        return true;
     }
     public static void startingAgents(String lvl, String url, List<Integer> agents, String path) {
 
@@ -29,4 +69,9 @@ public class Agents {
             System.out.println(key + " | " + value.get("first") + " | " + value.get("second"));
         }
     }
+    public static void cpuAgent(String path) {}
+    public static void memoryAgent(String path) {}
+    public static void networkAgent(String url, String path) {}
+    public static void specialAgent(String lvl, String path) {}
+    public static void startBot(List<Integer> agents, String configPath, String path) {}
 }
